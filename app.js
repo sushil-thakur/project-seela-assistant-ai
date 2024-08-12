da95558163038a7da00562a3c15f06fea74fa135
sushil <sushilt2059@gmail.com>
2024-08-12 15:38:34 +0545

    third commit

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 //elements
 const startBtn =document.querySelector("#start");
 const stopBtn =document.querySelector("#stop");
+const speakBtn =document.querySelector("#speak");
+
 
 
 //speech reconiaztion
@@ -15,6 +17,8 @@ recognition.onend=function(){
     console.log("vr deactive");
 };
 
+recognition.continuous=true;
+
 startBtn.addEventListener("click",()=>{
     recognition.start();
 
@@ -24,3 +28,23 @@ stopBtn.addEventListener("click",()=>{
     recognition.stop();
 
 });
+
+// seela speak
+function readOut(message) {
+    const speech = new SpeechSynthesisUtterance();
+    //different voice
+    const allVoice=speechSynthesis.getVoices()
+    speech.text = message;
+    speech.voice=allVoice[2];
+    speech.volume = 1;
+    window.speechSynthesis.speak(speech);
+    console.log("Speaking out:", message);
+}
+
+speakBtn.addEventListener("click", () => {
+    readOut("Hello, I am Sela, an advanced AI assistant. How can I help you, sir?");
+});
+
+window.onload=function(){
+    readOut("... ");
+};
